59619aa6c9563e0560782a2982b944ec4b187d50
hebingchang <hebingchang1@live.com>
2024-02-21 02:05:41 +0900

    dep: downgrade framer-motion

--- src/components/photo_modal.tsx
+++ src/components/photo_modal.tsx
@@ -36,26 +36,6 @@ export default function PhotoModal(props: PhotoModalProps) {
       classNames={{
         closeButton: 'z-20'
       }}
-      motionProps={isDesktop ? {
-        variants: {
-          enter: {
-            transform: "scale(1)",
-            opacity: 1,
-            transition: {
-              duration: 0.2,
-              ease: [0, 0, 0.2, 1],
-            },
-          },
-          exit: {
-            transform: "scale(1.03)",
-            opacity: 0,
-            transition: {
-              duration: 0.1,
-              ease: [0.4, 0, 1, 1],
-            },
-          },
-        }
-      } : undefined}
     >
       <ModalContent className='overflow-hidden'>
         {() => (
